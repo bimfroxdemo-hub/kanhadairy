5ddad68d5c4826f1932a7ec2aceedb2cc7c5506d
vimalsahu07 <bloger24x7@gmail.com>
2025-11-21 14:57:16 +0530

    navbar

--- dairy/src/componant/ui/Navbar.jsx
+++ dairy/src/componant/ui/Navbar.jsx
@@ -1,53 +1,28 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
-      <div className="navbar-container">
-        
+      {/* Main Navbar */}
+      <div className="flex justify-between items-center px-18 py-3 shadow">
+
         {/* Logo Left */}
-        <div className="logo-box">
-          <img src="../assets/logo.png" alt="logo" className="logo" />
+        <div>
+          <img 
+            src="/logo.png" 
+            alt="logo" 
+            className="w-[100px]"
+          />
         </div>
 
         {/* Links Right */}
-        <div className="navbar">
-          <NavLink 
-            to="/" 
-            className={({ isActive }) => (isActive ? "active-link" : "")}
-          >
-            Home
-          </NavLink>
-
-          <NavLink 
-            to="/about" 
-            className={({ isActive }) => (isActive ? "active-link" : "")}
-          >
-            About
-          </NavLink>
-
-          <NavLink 
-            to="/product" 
-            className={({ isActive }) => (isActive ? "active-link" : "")}
-          >
-            Product
-          </NavLink>
-
-          <NavLink 
-            to="/contact" 
-            className={({ isActive }) => (isActive ? "active-link" : "")}
-          >
-            Contact
-          </NavLink>
-
-          <NavLink 
-            to="/order" 
-            className={({ isActive }) => (isActive ? "active-link" : "")}
-          >
-            Order
-          </NavLink>
+        <div className="flex gap-10 text-lg font-medium">
+          <Link className="nav-link  hover:text-[#303983]" to="/">Home</Link>
+          <Link className="nav-link  hover:text-[#303983]" to="/about">About</Link>
+          <Link className="nav-link  hover:text-[#303983]" to="/product">Product</Link>
+          <Link className="nav-link  hover:text-[#303983]" to="/contact">Contact</Link>
+          <Link className="nav-link  hover:text-[#303983]" to="/order">Order</Link>
         </div>
 
       </div>
